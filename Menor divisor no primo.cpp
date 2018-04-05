6f134fc8c498460c690b01150afb34a624b232a4
brandon-glem <31105149+brandon-glem@users.noreply.github.com>
2018-04-05 09:12:33 -0500

    Menor div. no primo

---
+++ Menor divisor no primo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numero, divisores=0,divisor;
+	cout<<"ingrese numero: ";
+	cin>>numero;
+	for(int i=1;i<(numero+1);i=i+1){
+		if ((numero%i)==0){
+			divisores=divisores+1;
+			if(divisores==2){
+				divisor=i;
+			}
+		}
+	}
+	if(divisores!=2){
+		cout<<"no es primo"<<endl;
+		cout<<"su menor divisor es: "<<divisor;
+	}
+	return 0;
+}
+

ddb23c19f671bb63068e6c99c48d64c45b6e1a50
brandon-glem <31105149+brandon-glem@users.noreply.github.com>
2018-04-24 11:23:53 -0500

    arreglos sin punteros

--- Arreglos.cpp
+++ Arreglos.cpp
@@ -11,13 +11,11 @@ int suma(const int lista[],int tam){
 
 
 int suma_recu(const int lista[],int tam){
-	int r = 0;
 	if((tam-1)==0){
-		r += lista[0];
+		return lista[0];
 	}
 	else
-		r = lista[tam-1] + suma(lista,tam-1);
-	return r;
+		return lista[tam-1] + suma(lista,--tam);
 }
 
 void reverza(int lista[],int tam){
@@ -26,17 +24,51 @@ void reverza(int lista[],int tam){
 		ulti=lista[i];
 		lista[i]=lista[indi];
 		lista[indi]=ulti;
-		indi=indi-1;
+		indi-=1;
 	}
 }
 
-void lend(int lista[]){
-	
+int lend(char *lista){
+	int len=0;
+	for(int i=0;lista[i]!='\0';i++){
+		++len;
+	}
+	return len;
+}
+
+void copiar(char *lista1,char *lista2){
+	for(int i=0;lista1[i]!='\0';i++){
+		lista2[i]=lista1[i];
+	}
+}
+
+void concatenar(char *lista1,char *lista2){
+	int cont=0;
+	while(lista1[cont]!='\0')
+		cont++;
+	for(int i=0;lista2[i]!='\0';i++){
+		lista1[cont]=lista2[i];
+		cont++;
+	}
 }
 
 int main() {
 	int matriz[]={1,2,3,4,5},tam=5;
-	cout<<suma(matriz,5);
+	cout<<suma_recu(matriz,5)<<endl;
+	
+	int matriz2[]={1,2,3,4,5},tam1=5;
+	reverza(matriz2,tam1);
+	for(int j=0;j<tam1;j++){
+		cout<<matriz2[j];
+	}
+	cout<<'\n';
+	
+	char palabra[]="holalalgd";
+	cout<< lend(palabra)<<endl;
+	
+	char hi[]="idfsnsfr";
+	copiar(palabra,hi);
+	cout<<palabra;
 	return 0;
 }
 
